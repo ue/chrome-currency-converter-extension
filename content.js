47a0f809c307568b9b9066db0ab9ff98a83bd3e4
ue <ugurerdal93@gmail.com>
2018-08-13 17:47:27 +0300

    changed currency symbol

--- content.js
+++ content.js
@@ -8,27 +8,30 @@ if (range) {
   selectedText = div.innerHTML;
 }
 
-if (selectedText.length > 1 && selectedText.includes('$') ||
-    selectedText.includes('₺') ||
-    selectedText.includes('TL')) {
-    var result = decideCalculate(selectedText);
+if (
+  (selectedText.length > 1 && selectedText.includes('$')) ||
+  selectedText.includes('₺') ||
+  selectedText.includes('TL')
+) {
+  var result = decideCalculate(selectedText);
 
-    chrome.runtime.sendMessage({
-      title: document.title,
-      url: window.location.href,
-      summary: result !== 'NaN$' ? result : 'Oops!',
-    });
-
+  chrome.runtime.sendMessage({
+    title: document.title,
+    url: window.location.href,
+    summary: result !== 'NaN$' ? result : 'Oops!'
+  });
 } else {
-    chrome.runtime.sendMessage({
-      summary: "Please select...",
+  chrome.runtime.sendMessage({
+    summary: 'Please select...'
   });
 }
 
 function seperateSelectedText(selectedText) {
   var isBlock = selectedText.includes('<');
   var seperateFromMoneyIcon = selectedText.split('$');
-  return isBlock ? seperateFromMoneyIcon[1].split('</')[0] : seperateFromMoneyIcon[1];
+  return isBlock
+    ? seperateFromMoneyIcon[1].split('</')[0]
+    : seperateFromMoneyIcon[1];
 }
 
 function decideCalculate(selectedText) {
@@ -36,24 +39,28 @@ function decideCalculate(selectedText) {
   var _currencyValue = getCurrencyValueFromApi();
   var res;
 
-  if(selectedText.includes('$')) {
-    return (_currencyValue * pureSelectedCurrenct).toFixed(2) + '$';
+  if (selectedText.includes('$')) {
+    return (_currencyValue * pureSelectedCurrenct).toFixed(2) + '₺';
   } else if (selectedText.includes('TL') || selectedText.includes('₺')) {
-    return (pureSelectedCurrenct / _currencyValue).toFixed(2) + '₺';
+    return (pureSelectedCurrenct / _currencyValue).toFixed(2) + '$';
   }
 }
 
 function getCurrencyValueFromApi() {
   var xmlhttp = new XMLHttpRequest();
-  xmlhttp.open('GET', 'https://free.currencyconverterapi.com/api/v5/convert?q=USD_TRY&compact=y', true);
+  xmlhttp.open(
+    'GET',
+    'https://free.currencyconverterapi.com/api/v5/convert?q=USD_TRY&compact=y',
+    true
+  );
 
   xmlhttp.onreadystatechange = function() {
-      if (xmlhttp.readyState == 4) {
-          if(xmlhttp.status == 200) {
-              value =  JSON.parse(xmlhttp.responseText).USD_TRY.val;
-          }
+    if (xmlhttp.readyState == 4) {
+      if (xmlhttp.status == 200) {
+        value = JSON.parse(xmlhttp.responseText).USD_TRY.val;
       }
-    };
+    }
+  };
 
   xmlhttp.send(null);
   return value;
